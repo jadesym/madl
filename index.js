c7515c43bd0c38fa3ed7ea5c924b54fcabc45395
Kevin Fu <kevin.fu808@gmail.com>
2020-05-24 16:57:38 -0700

    Starting empty implementation to be imported

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Starting empty implementation for the index.js file
+export const printMessage = () =>
+  console.log(
+    "Thanks for using the madl module. Development is currently in progres!"
+  );
