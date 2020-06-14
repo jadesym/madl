a928e6627c0efed1318439bd37fbbd8d2a09f415
Kevin Fu <kevin.fu808@gmail.com>
2020-06-13 21:55:36 -0700

    Adding type annotation for printMessage function

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 // Starting empty implementation for the index.js file
-export const printMessage = () => 'Thanks for using the madl module. Development is currently in progres!';
+export const printMessage = (message: string) => `Thanks for using the madl module. Development is currently in progress! Here is your input message: ${message}`;
